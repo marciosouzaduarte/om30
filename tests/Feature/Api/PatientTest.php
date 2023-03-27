75a3723e09d2495e56556964b4f2c230923ba3b6
marciosouzaduarte <souzaduarte@gmail.com>
2023-03-27 10:49:01 -0300

    add tests

--- tests/Feature/Api/PatientTest.php
+++ tests/Feature/Api/PatientTest.php
@@ -12,16 +12,13 @@ class PatientTest extends TestCase
     public function test_list_all_patients(): void
     {
         $response = $this->getJson('/patient/list');
-
         $response->assertStatus(200);
     }
 
     public function test_get_count_patients(): void
     {
         Patient::factory(20)->create();
-
         $response = $this->getJson('/patient/list');
-
         //$response->dump();
         $response->assertJsonCount(10, 'data');
         $response->assertStatus(200);
@@ -38,25 +35,14 @@ class PatientTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_validate_create_patient() {
+    public function test_fail_create_patient() {
         $patient = Patient::factory()->create();
         $response = $this->postJson("/patient", []);
         $response->assertStatus(400);
     }
 
-    public function test_validate_update_patient() {
-        $patient = Patient::factory()->create();
-        $response = $this->putJson("/patient/{$patient->uuid}", [
-            'name' => 'Airton Senna'
-        ]);
-        $response->assertStatus(204);
-    }
-
     public function test_create_patient() {
-        $patient = Patient::factory()->create();
-
         $faker = Factory::create();
-        
         $response = $this->postJson("/patient", [
             'uuid'=> $faker->uuid(),
             'name' => $faker->name(),
@@ -68,5 +54,20 @@ class PatientTest extends TestCase
             'photo' => '/storage/app/public/users/' . md5(Str::random()) . '.png'
         ]);
         $response->assertStatus(201);
+    }
+
+    public function test_delete_patient() {
+        $patient = Patient::factory()->create();
+        $response = $this->deleteJson("/patient/{$patient->uuid}");
+        $response->assertStatus(204);
+    }
+
+    public function test_update_patient() {
+        $patient = Patient::factory()->create();
+        $faker = Factory::create();
+        $response = $this->putJson("/patient/{$patient->uuid}", [
+            'name' => $faker->name()
+        ]);
+        $response->assertStatus(204);
     }
 }
